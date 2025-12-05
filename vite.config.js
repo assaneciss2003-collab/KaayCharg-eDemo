6b238185f5134db0fa588d88db643e321bafa03c
assaneciss2003-collab <assaneciss2003@gmail.com>
2025-12-05 23:20:08 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/kaaychargé/',  // Ajoutez cette ligne !
   server: {
     port: 5173,
     open: true
